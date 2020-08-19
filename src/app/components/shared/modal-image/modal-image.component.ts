26e735bd0dcfc3eed5d43cff38a525e04ecb6455
Felipe Fabossi Costa <felipefabossicosta@MBP-de-Felipe.lan>
2020-08-19 18:48:39 -0300

    [FABOSSI] .

--- src/app/components/shared/modal-image/modal-image.component.ts
+++ src/app/components/shared/modal-image/modal-image.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { animatedGalleryItems } from '../../chat/chat-body/chat-body-animations';
 import { ChatService } from 'src/app/services/chat.service';
 
@@ -9,14 +9,23 @@ import { viewAllImagesTrigger, viewAllImagesItems } from './modalImage-animation
   styleUrls: ['./modal-image.component.scss'],
   animations: [viewAllImagesTrigger, animatedGalleryItems, viewAllImagesItems]
 })
-export class ModalImageComponent {
+export class ModalImageComponent implements OnInit {
 
   images = [];
   imageInView = { src: '', title: '' };
   invert = false;
-
+  srcImage = '../../../../assets/plus-b.svg';
+  height = '25';
+  width = '10';
   constructor(private chatService: ChatService) { }
 
+  ngOnInit() {
+    this.chatService.closeInfo.subscribe((status) => {
+      if (status) {
+        this.invert = true;
+      }
+    });
+  }
 
   closeModal() {
     this.chatService.viewAllImages.next(false);
@@ -29,5 +38,7 @@ export class ModalImageComponent {
   currentImgInfo(id) {
     this.imageInView = this.chatService.images[id];
   }
+
+  loadMore() { }
 
 }
